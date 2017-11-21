2e3105eea1de271cb7f154ebd5d8c5ebca22cb17
Leonardo Dino <leonardo@kunstdesign.com.br>
2017-11-20 23:53:50 -0200

    add example

---
+++ example/App.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import DatePicker from './lib/DatePicker'
+const NEUTRAL_STATE_COLOR = '#D5D8DB'
+
+export default class App extends React.Component {
+  state = {date: new Date(), birthday: null}
+  render() {
+    const {date, birthday} = this.state
+    const customStyles = {
+      dateInput: styles.dateInput,
+      dateText: styles.dateText,
+      placeholderText: styles.placeholderText
+    }
+    return (
+      <View style={styles.container}>
+        <DatePicker
+          style={styles.inputRow}
+          customStyles={customStyles}
+          mode='date'
+          placeholder='birthday'
+          date={birthday}
+          onDateChange={birthday => this.setState({birthday})}
+        />
+        <DatePicker
+          style={styles.inputRow}
+          customStyles={customStyles}
+          mode='datetime'
+          date={date}
+          onDateChange={date => this.setState({date})}
+        />
+        <DatePicker
+          style={styles.inputRow}
+          customStyles={customStyles}
+          mode='time'
+          date={date}
+          onDateChange={date => this.setState({date})}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 32,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRow: {
+    width: '100%',
+    flexDirection: 'row',
+    borderBottomWidth: 2,
+    borderBottomColor: NEUTRAL_STATE_COLOR,
+    paddingBottom: 8,
+    minHeight: 48,
+  },
+  dateInput: {
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    paddingTop: 16,
+    paddingBottom: 2,
+    borderWidth: 0,
+    alignSelf: 'flex-end',
+    flex: 1,
+  },
+  dateText: {
+    fontSize: 16,
+    color: '#313233',
+    textDecorationLine: 'none',
+  },
+  placeholderText: {
+    fontSize: 16,
+    color: '#c9c9c9'
+  },
+})
